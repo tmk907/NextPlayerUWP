9ecb7409053253558be2fbfeb88747568ebe0540
Tomasz Walecki <tomek_w@outlook.com>
2016-05-30 10:34:31 +0200

    change jamendo radio

---
+++ NextPlayerUWPDataLayer/Jamendo/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NextPlayerUWPDataLayer.Jamendo.Models
+{
+    public class Response<TResult>
+    {
+        [JsonProperty("headers")]
+        public Headers Headers { get; set; }
+
+        [JsonProperty("results")]
+        public IList<TResult> Results { get; set; }
+    }
+}
