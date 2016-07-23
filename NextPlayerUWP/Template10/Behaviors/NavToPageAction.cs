f2726eddb6fe1e233364c2f00d3c381440282ece
Tomasz Walecki <tomek_w@outlook.com>
2016-07-23 16:28:09 +0200

    added template10 project, with theme changing fix

---
+++ NextPlayerUWP/Template10/Behaviors/NavToPageAction.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Services.NavigationService;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Template10.Behaviors
+{
+    public class NavToPageAction : DependencyObject, IAction
+    {
+        public object Execute(object sender, object parameter)
+        {
+            if (null == Page)
+                return false;
+
+            if (Frame == null)
+                throw new NullReferenceException($"{nameof(NavToPageAction)}.{nameof(Frame)} is required.");
+
+            var nav = NavigationService.GetForFrame(Frame);
+            if (nav == null)
+                throw new NullReferenceException($"Cannot find NavigationService for {Frame.ToString()}.");
+
+            nav.Navigate(Page as Type, Parameter, InfoOverride);
+            return null;
+        }
+
+        public Frame Frame
+        {
+            get { return (Frame)GetValue(FrameProperty); }
+            set { SetValue(FrameProperty, value); }
+        }
+        public static readonly DependencyProperty FrameProperty =
+            DependencyProperty.Register(nameof(Frame), typeof(Frame),
+                typeof(NavToPageAction), new PropertyMetadata(null));
+
+        public object Page
+        {
+            get { return (object)GetValue(PageProperty); }
+            set { SetValue(PageProperty, value); }
+        }
+        public static readonly DependencyProperty PageProperty =
+            DependencyProperty.Register(nameof(Page), typeof(object),
+                typeof(NavToPageAction), new PropertyMetadata(null));
+
+        public object Parameter
+        {
+            get { return (object)GetValue(ParameterProperty); }
+            set { SetValue(ParameterProperty, value); }
+        }
+        public static readonly DependencyProperty ParameterProperty =
+            DependencyProperty.Register(nameof(Parameter), typeof(object),
+                typeof(NavToPageAction), new PropertyMetadata(null));
+
+        public NavigationTransitionInfo InfoOverride
+        {
+            get { return (NavigationTransitionInfo)GetValue(InfoOverrideProperty); }
+            set { SetValue(InfoOverrideProperty, value); }
+        }
+        public static readonly DependencyProperty InfoOverrideProperty =
+            DependencyProperty.Register(nameof(InfoOverride), typeof(NavigationTransitionInfo),
+                typeof(NavToPageAction), new PropertyMetadata(null));
+    }
+}
---
+++ NextPlayerUWP/Template10/Services/NavigationService/NavigatingEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using Template10.Common;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Template10.Services.NavigationService
+{
+    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-NavigationService
+    public class NavigatingEventArgs : NavigatedEventArgs
+    {
+        DeferralManager Manager;
+        public Deferral GetDeferral() => Manager.GetDeferral();
+
+        public NavigatingEventArgs(DeferralManager manager) : base()
+        {
+            Manager = manager;
+        }
+
+        public NavigatingEventArgs(DeferralManager manager, NavigatingCancelEventArgs e, Page page, object parameter) : this(manager)
+        {
+            NavigationMode = e.NavigationMode;
+            PageType = e.SourcePageType;
+            Page = page;
+            Parameter = parameter;
+        }
+
+        public bool Cancel { get; set; } = false;
+        public bool Suspending { get; set; } = false;
+    }
+}
