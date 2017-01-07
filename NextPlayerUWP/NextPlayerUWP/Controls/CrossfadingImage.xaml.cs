5754246729a98572f6a930cc736cde27df4f53c7
Tomasz Walecki <next-player@outlook.com>
2017-01-07 17:33:30 +0100

    change to CrossfadingImage

---
+++ NextPlayerUWP/NextPlayerUWP/Controls/CrossfadingImage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NextPlayerUWP.Controls
+{
+    public sealed partial class CrossfadingImage : UserControl
+    {
+        public CrossfadingImage()
+        {
+            this.InitializeComponent();
+            imageTop.ImageOpened += ImageTop_ImageOpened;
+            imageBottom.ImageOpened += ImageBottom_ImageOpened;
+        }
+
+        private void ImageBottom_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            BottomImageShow.Begin();
+            TopImageHide.Begin();
+        }
+
+        private void ImageTop_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            TopImageShow.Begin();
+            BottomImageHide.Begin();
+        }
+
+        public Stretch Stretch
+        {
+            get { return (Stretch)GetValue(StretchProperty); }
+            set
+            {
+                SetValue(StretchProperty, value);
+                OnStretchChanged();
+            }
+        }
+
+        private void OnStretchChanged()
+        {
+            imageBottom.Stretch = Stretch;
+            imageTop.Stretch = Stretch;
+        }
+
+        public static readonly DependencyProperty StretchProperty =
+            DependencyProperty.Register("Stretch", typeof(Stretch), typeof(CrossfadingImage), null);
+
+        public Uri ImageUri
+        {
+            get { return (Uri)GetValue(ImageUriProperty); }
+            set
+            {
+                SetValue(ImageUriProperty, value);
+                OnImageUriChange();
+            }
+        }
+
+        public static readonly DependencyProperty ImageUriProperty =
+            DependencyProperty.Register("ImageUri", typeof(Uri), typeof(CrossfadingImage), null);
+
+        private void OnImageUriChange()
+        {
+            if (ImageUri == null) return;
+            if (imageTop.Opacity == 0)
+            {
+                BitmapImage bmp = new BitmapImage(ImageUri);
+                imageTop.Source = bmp;
+            }
+            else
+            {
+                BitmapImage bmp = new BitmapImage(ImageUri);
+                imageBottom.Source = bmp;
+            }
+        }
+
+        public TimeSpan FadeInDuration
+        {
+            get { return (TimeSpan)GetValue(FadeInDurationProperty); }
+            set
+            {
+                SetValue(FadeInDurationProperty, value);
+                TopImageShowAnimation.Duration = value;
+                BottomImageShowAnimation.Duration = value;
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for FadeInDuration.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FadeInDurationProperty =
+            DependencyProperty.Register("FadeInDuration", typeof(TimeSpan), typeof(CrossfadingImage), new PropertyMetadata(TimeSpan.FromSeconds(1)));
+
+
+        public TimeSpan FadeOutDuration
+        {
+            get { return (TimeSpan)GetValue(FadeOutDurationProperty); }
+            set
+            {
+                SetValue(FadeOutDurationProperty, value);
+                TopImageHideAnimation.Duration = value;
+                BottomImageHideAnimation.Duration = value;
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for FadeOutDuration.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FadeOutDurationProperty =
+            DependencyProperty.Register("FadeOutDuration", typeof(TimeSpan), typeof(CrossfadingImage), new PropertyMetadata(TimeSpan.FromSeconds(1)));
+
+    }
+}
--- NextPlayerUWP/NextPlayerUWP/Views/NowPlayingView.xaml.cs
+++ NextPlayerUWP/NextPlayerUWP/Views/NowPlayingView.xaml.cs
@@ -1,18 +1,10 @@
-﻿using Microsoft.Graphics.Canvas.Effects;
-using Microsoft.Toolkit.Uwp.UI.Animations;
-using NextPlayerUWP.Common;
+﻿using NextPlayerUWP.Common;
 using NextPlayerUWP.ViewModels;
 using NextPlayerUWPDataLayer.Constants;
 using System;
-using System.Numerics;
-using Windows.Foundation;
-using Windows.Graphics.Effects;
-using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,11 +15,6 @@ namespace NextPlayerUWP.Views
     /// </summary>
     public sealed partial class NowPlayingView : Page
     {
-        Compositor _compositor;
-        CompositionEffectBrush _crossFadeBrushPrimary;
-        CompositionSurfaceBrush _previousSurfaceBrushPrimary;
-        CompositionScopedBatch _crossFadeBatchPrimary;
-
         Uri imageUri;
         NowPlayingViewModel ViewModel;
         PointerEventHandler pointerpressedhandler;
@@ -49,61 +36,19 @@ namespace NextPlayerUWP.Views
             Init();
             if (ViewModel.CoverUri != null)
             {
-                if (first)
-                {
-                    imageUri = GetAlbumUri();
-                    PrimaryImage.Source = imageUri;
-                    first = false;
-                }
+                imageUri = GetAlbumUri();
+                PrimaryImage.ImageUri = imageUri;
             }
             ViewModel.PropertyChanged += ViewModel_PropertyChanged;
         }
 
         private void Init()
         {
-            first = true;
-            _compositor = ElementCompositionPreview.GetElementVisual(PortraitGrid).Compositor;
-            PrimaryImage.ImageOpened += PrimaryImage_FirstOpened;
-            PrimaryImage.PlaceholderDelay = TimeSpan.FromMilliseconds(-1);
-            PrimaryImage.PlaceholderDelay = TimeSpan.FromMilliseconds(-1);
-            PrimaryImage.SharedSurface = true;
-
-            // Create a crossfade brush to animate image transitions
-            IGraphicsEffect graphicsEffect = new ArithmeticCompositeEffect()
-            {
-                Name = "CrossFade",
-                Source1Amount = 0,
-                Source2Amount = 1,
-                MultiplyAmount = 0,
-                Source1 = new CompositionEffectSourceParameter("ImageSource"),
-                Source2 = new CompositionEffectSourceParameter("ImageSource2"),
-            };
-            CompositionEffectFactory factory = _compositor.CreateEffectFactory(graphicsEffect, new[] { "CrossFade.Source1Amount", "CrossFade.Source2Amount" });
-            _crossFadeBrushPrimary = factory.CreateBrush();
         }
 
         private void NowPlayingView_Unloaded(object sender, RoutedEventArgs e)
         {
             ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
-            PrimaryImage.ImageOpened -= PrimaryImage_FirstOpened;
-            PrimaryImage = null;
-            _compositor = null;
-            if (_crossFadeBatchPrimary != null)
-            {
-                _crossFadeBatchPrimary.Dispose();
-                _crossFadeBatchPrimary = null;
-
-            }
-            if (_crossFadeBrushPrimary != null)
-            {
-                _crossFadeBrushPrimary.Dispose();
-                _crossFadeBrushPrimary = null;
-            }
-            if (_previousSurfaceBrushPrimary != null)
-            {
-                _previousSurfaceBrushPrimary.Dispose();
-                _previousSurfaceBrushPrimary = null;
-            }
             UnloadSliderEvents();
         }
 
@@ -158,134 +103,13 @@ namespace NextPlayerUWP.Views
             }
         }
 
-        bool first;
         private void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(ViewModel.CoverUri))
             {
                 imageUri = GetAlbumUri();
-                if (first)
-                {
-                    PrimaryImage.Source = imageUri;
-                    first = false;
-                }
-                else
-                {
-                    StartAnimations();
-                }
+                PrimaryImage.ImageUri = imageUri;
             }
         }
-
-        private void StartAnimations()
-        {
-            if (_previousSurfaceBrushPrimary == null) return;
-            if (_crossFadeBatchPrimary == null)
-            {
-                // Save the previous image for a cross-fade
-                _previousSurfaceBrushPrimary.Surface = PrimaryImage.SurfaceBrush.Surface;
-                _previousSurfaceBrushPrimary.CenterPoint = PrimaryImage.SurfaceBrush.CenterPoint;
-                _previousSurfaceBrushPrimary.Stretch = PrimaryImage.SurfaceBrush.Stretch;
-
-                // Load the new background image
-                PrimaryImage.ImageOpened += PrimaryImage_ImageChanged;
-            }
-
-            // Update the images
-            PrimaryImage.Source = imageUri;
-        }
-
-        private void PrimaryImage_ImageChanged(object sender, RoutedEventArgs e)
-        {
-            if (_crossFadeBatchPrimary == null)
-            {
-                TimeSpan duration = TimeSpan.FromMilliseconds(1000);
-
-                // Create the animations for cross-fading
-                ScalarKeyFrameAnimation fadeInAnimation = _compositor.CreateScalarKeyFrameAnimation();
-                fadeInAnimation.InsertKeyFrame(0, 0);
-                fadeInAnimation.InsertKeyFrame(1, 1);
-                fadeInAnimation.Duration = duration;
-
-                ScalarKeyFrameAnimation fadeOutAnimation = _compositor.CreateScalarKeyFrameAnimation();
-                fadeOutAnimation.InsertKeyFrame(0, 1);
-                fadeOutAnimation.InsertKeyFrame(1, 0);
-                fadeOutAnimation.Duration = duration;
-
-                // Create a batch object so we can cleanup when the cross-fade completes.
-                _crossFadeBatchPrimary = _compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
-
-                // Set the sources
-                _crossFadeBrushPrimary.SetSourceParameter("ImageSource", PrimaryImage.SurfaceBrush);
-                _crossFadeBrushPrimary.SetSourceParameter("ImageSource2", _previousSurfaceBrushPrimary);
-
-                // Animate the source amounts to fade between
-                _crossFadeBrushPrimary.StartAnimation("CrossFade.Source1Amount", fadeInAnimation);
-                _crossFadeBrushPrimary.StartAnimation("CrossFade.Source2Amount", fadeOutAnimation);
-
-                // Update the image to use the cross fade brush
-                PrimaryImage.Brush = _crossFadeBrushPrimary;
-
-                _crossFadeBatchPrimary.Completed += PrimaryBatch_CrossFadeCompleted;
-                _crossFadeBatchPrimary.End();
-            }
-
-            // Unhook the handler
-            PrimaryImage.ImageOpened -= PrimaryImage_ImageChanged;
-        }
-
-        private void PrimaryBatch_CrossFadeCompleted(object sender, CompositionBatchCompletedEventArgs args)
-        {
-            PrimaryImage.Brush = PrimaryImage.SurfaceBrush;
-            // Dispose the image
-            ((CompositionDrawingSurface)_previousSurfaceBrushPrimary.Surface).Dispose();
-            _previousSurfaceBrushPrimary.Surface = null;
-
-            // Clear out the batch
-            _crossFadeBatchPrimary = null;
-        }
-
-        private void PrimaryImage_FirstOpened(object sender, RoutedEventArgs e)
-        {
-            ScalarKeyFrameAnimation fadeInAnimation = _compositor.CreateScalarKeyFrameAnimation();
-            fadeInAnimation.InsertKeyFrame(0, 0);
-            fadeInAnimation.InsertKeyFrame(1, 1);
-            fadeInAnimation.Duration = TimeSpan.FromMilliseconds(1000);
-            PrimaryImage.SpriteVisual.StartAnimation("Opacity", fadeInAnimation);
-
-            CompositionDrawingSurface surface = (CompositionDrawingSurface)PrimaryImage.SurfaceBrush.Surface;
-            PrimaryImage.SurfaceBrush.CenterPoint = new Vector2((float)surface.Size.Width, (float)surface.Size.Height) * .5f;
-
-            _previousSurfaceBrushPrimary = _compositor.CreateSurfaceBrush();
-
-            PrimaryImage.ImageOpened -= PrimaryImage_FirstOpened;
-        }
-
-        //private void CoverImage_ImageOpened(object sender, RoutedEventArgs e)
-        //{
-        //    var anim1 = CoverImage.Fade(1, 400, 0);
-        //    var anim2 = CoverImage2.Fade(0, 500, 0);
-        //    anim2.Completed += Anim2_Completed;
-        //    anim1.Completed += Anim1_Completed;
-        //    anim1.Start();
-        //    anim2.Start();
-        //}
-
-        //private void Anim1_Completed(object sender, EventArgs e)
-        //{
-        //    System.Diagnostics.Debug.WriteLine("Anim1_Completed");
-        //}
-
-        //private void Anim2_Completed(object sender, EventArgs e)
-        //{
-        //    System.Diagnostics.Debug.WriteLine("Anim2_Completed");
-
-        //    BitmapImage source1 = CoverImage.Source as BitmapImage;
-        //    BitmapImage bmp = new BitmapImage(source1.UriSource);
-        //    bmp.DecodePixelHeight = source1.DecodePixelHeight;
-        //    bmp.DecodePixelWidth = source1.DecodePixelWidth;
-        //    CoverImage2.Source = bmp;
-        //    CoverImage2.Opacity = 1;
-        //    CoverImage.Opacity = 0;
-        //}
     }    
 }
